5ddfcb321f523590e77ae247acc0d309a797258d
Niga9L <59453024+Niga9L@users.noreply.github.com>
2020-06-24 12:54:36 +0300

    Delete index.js

--- keys/index.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    MONGODB_URL: 'mongodb+srv://Niga9L:nZ90JfXTL5TINbhu@cluster0-csbuk.mongodb.net/shop',
-    SECRET_VALUE: 'some secret value',
-    SENDGRID_API_KEY: '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',
-    EMAIL_FROM: 'lyaginvanya@gmail.com',
-    BASE_URL: 'http://localhost:3000',
-    DDDD: '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'
-
-}
